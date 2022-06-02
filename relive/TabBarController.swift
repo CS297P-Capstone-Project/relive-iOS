4e5ca21a0b885fc71b013c9e368fde33bdbb4850
tanyalohiya <tanyalohiya@gmail.com>
2022-06-01 19:24:31 -0700

    Tab Bar Controller

---
+++ relive/TabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarController.swift
+//  relive
+//
+//  Created by Tanya Lohiya on 5/31/22.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        tabBar.barTintColor = .white
+        //tabBar.isTranslucent = false
+        tabBar.tintColor = .red
+//        tabBar.unselectedItemTintColor = .blue
+
+    }
+
+}
